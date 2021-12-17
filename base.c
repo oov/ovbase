73fa6d2aef35023b5fd3acd8f21a09d690bde89b
oov <ilovevipper@gmail.com>
2021-12-18 08:18:30 +0900

    add threads.h

--- base.c
+++ base.c
@@ -24,32 +24,7 @@
 #include <stdatomic.h>
 static _Atomic uint64_t g_global_hint = 0;
 
-#if __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
-#include <threads.h>
-#else
-
-#ifdef __GNUC__
-
-#pragma GCC diagnostic push
-#if __has_warning("-Wreserved-macro-identifier")
-#pragma GCC diagnostic ignored "-Wreserved-macro-identifier"
-#endif
-#if __has_warning("-Wpadded")
-#pragma GCC diagnostic ignored "-Wpadded"
-#endif
-#if __has_warning("-Wmissing-noreturn")
-#pragma GCC diagnostic ignored "-Wmissing-noreturn"
-#endif
-#include "3rd/threads/threads.h"
-#pragma GCC diagnostic pop
-
-#else
-
-#include "3rd/threads/threads.h"
-
-#endif // __GNUC__
-
-#endif // __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
+#include "threads.h"
 
 #ifdef __GNUC__
 
