fdff63a88590a0bc9ee0b5aa149abd62132999c0
oov <ilovevipper@gmail.com>
2021-12-24 06:20:04 +0900

    add cndvar

--- threads.h
+++ threads.h
@@ -30,3 +30,49 @@
 #endif // __GNUC__
 
 #endif // __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
+
+struct cndvar {
+  cnd_t cnd;
+  mtx_t mtx;
+  int var;
+};
+
+static inline void cndvar_init(struct cndvar *const cv) {
+  mtx_init(&cv->mtx, mtx_plain | mtx_recursive);
+  cnd_init(&cv->cnd);
+  cv->var = 0;
+}
+
+static inline void cndvar_exit(struct cndvar *const cv) {
+  cnd_destroy(&cv->cnd);
+  mtx_destroy(&cv->mtx);
+}
+
+static inline void cndvar_lock(struct cndvar *const cv) { mtx_lock(&cv->mtx); }
+
+static inline void cndvar_unlock(struct cndvar *const cv) { mtx_unlock(&cv->mtx); }
+
+static inline void cndvar_signal(struct cndvar *const cv, int const var) {
+  cv->var = var;
+  cnd_signal(&cv->cnd);
+}
+
+static inline int cndvar_wait_while(struct cndvar *cv, int var) {
+  while (cv->var == var) {
+    int const r = cnd_wait(&cv->cnd, &cv->mtx);
+    if (r != thrd_success) {
+      return r;
+    }
+  }
+  return thrd_success;
+}
+
+static inline int cndvar_timedwait_while(struct cndvar *cv, int var, const struct timespec *abs_time) {
+  while (cv->var == var) {
+    int const r = cnd_timedwait(&cv->cnd, &cv->mtx, abs_time);
+    if (r != thrd_success) {
+      return r;
+    }
+  }
+  return thrd_success;
+}
