9c1520b1041be913fc5a34193ceb3fece5f33ccb
oov <ilovevipper@gmail.com>
2022-02-12 08:13:06 +0900

    fix str_cpy_ parameter

--- src/str/cpy_.c
+++ src/str/cpy_.c
@@ -2,7 +2,7 @@
 
 #ifdef USE_STR
 
-error str_cpy_(struct str *const s, const char *s2 MEM_FILEPOS_PARAMS) {
+error str_cpy_(struct str *const s, char const *const s2 MEM_FILEPOS_PARAMS) {
   if (!s || !s2) {
     return errg(err_invalid_arugment);
   }
